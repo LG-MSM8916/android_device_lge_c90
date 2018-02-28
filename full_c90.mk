eefb34f67f601ae9ff487e817c28686c661087b6
Vineeth Penugonda <vineeth.pen@outlook.com>
2018-02-28 23:24:14 +0530

    c90: Added c90's NFC and CAM stock files (Trying to fix NFC and CAMERA)
    Signed-off-by: Vineeth Penugonda <vineeth.pen@outlook.com>

--- full_c90.mk
+++ full_c90.mk
@@ -35,9 +35,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml
+
 # NFC
 PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
     NfcNci \
     nfc_nci.bcm2079x.default \
     Tag
