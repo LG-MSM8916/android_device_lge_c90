8519ef358488e648cedbef3c9723d86cd671da03
Vineeth Penugonda <vineeth.pen@gmail.com>
2018-10-18 15:27:53 -0500

    c90: Fix wrong dir name
    Signed-off-by: Vineeth Penugonda <vineeth.pen@gmail.com>

--- device.mk
+++ device.mk
@@ -41,7 +41,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # GPS
 PRODUCT_COPY_FILES += \
