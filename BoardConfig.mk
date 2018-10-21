c60c9347ef5798271c575ac0b1786a7458308856
dom133 <dom133_pl@protonmail.com>
2018-10-21 11:11:11 +0200

    c90: move init.*.rc to vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,4 +40,4 @@ TARGET_PROVIDES_LIBLIGHT := true
 BOARD_NFC_DEVICE := "/dev/bcm2079x"
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.recovery
